00ec929c56a6b4de93c63a88f1cd4bfdcda1b1b9
hqp001 <hqp001@bucknell.edu>
2026-05-15 01:35:51 -0400

    linuxremote3

--- config/prod.exs
+++ config/prod.exs
@@ -2,11 +2,7 @@ import Config
 
 config :csci379_final, Csci379FinalWeb.Endpoint,
   cache_static_manifest: "priv/static/cache_manifest.json",
-  check_origin: ["https://eg.bucknell.edu"],
-  force_ssl: [
-    rewrite_on: [:x_forwarded_proto],
-    exclude: [hosts: ["localhost", "127.0.0.1"]]
-  ]
+  check_origin: ["https://eg.bucknell.edu"]
 
 # Use local Swoosh adapter — no external email service needed
 config :swoosh, api_client: false
